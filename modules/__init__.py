de6486354b189230554cb00c9e87f9fe4c2794f3
Marble-Cost <wasesitowaginal@gmail.com>
2026-04-23 05:07:11 -0500

    Actualizar __init__.py

--- modules/__init__.py
+++ modules/__init__.py
@@ -3,11 +3,15 @@ from .quality import compute_quality_score, score_label
 from .dashboard import auto_dashboard, detect_column_types
 from .ai_narrator import generate_narrative, answer_natural_query
 from .report_generator import generate_pdf_report
-from .risk_detector import (
-    run_risk_analysis,
-    get_tercero_summary,
-    get_analysis_stats,
-    RISK_DICTIONARY_RAW,
-    RISK_COLORS,
-    ALERT_SCORES,
-)
+
+try:
+    from .risk_detector import (
+        run_risk_analysis,
+        get_tercero_summary,
+        get_analysis_stats,
+        RISK_DICTIONARY_RAW,
+        RISK_COLORS,
+        ALERT_SCORES,
+    )
+except ImportError:
+    pass
