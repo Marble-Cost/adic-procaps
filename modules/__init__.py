79da21ab59cde1b892bcbf76ba661d7147e1f55f
Marble-Cost <wasesitowaginal@gmail.com>
2026-04-23 05:01:55 -0500

    Actualizar __init__.py

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,6 +1,13 @@
-# modules package
 from .connector import load_from_upload, load_sample
 from .quality import compute_quality_score, score_label
 from .dashboard import auto_dashboard, detect_column_types
 from .ai_narrator import generate_narrative, answer_natural_query
 from .report_generator import generate_pdf_report
+from .risk_detector import (
+    run_risk_analysis,
+    get_tercero_summary,
+    get_analysis_stats,
+    RISK_DICTIONARY_RAW,
+    RISK_COLORS,
+    ALERT_SCORES,
+)
